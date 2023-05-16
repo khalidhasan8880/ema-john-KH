3c3c4bac04900a67ac9588f8f1e4cfaa1d25994a
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-05-16 14:58:51 +0600

    added server for ema-john client

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,17 +3,23 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
+    const {totalProducts} = useLoaderData()
+    const [selectedPageNumber, setSelectedPageNumber] = useState(0)
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
+    
+    const productPerPage = 10;
+    const totalPages = Math.ceil(totalProducts / productPerPage);
 
+    
     useEffect(() => {
-        fetch('products.json')
+        fetch(`http://localhost:5000/products?page=${selectedPageNumber}&limit=${productPerPage}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, []);
+    }, [productPerPage,selectedPageNumber]);
 
     useEffect(() => {
         const storedCart = getShoppingCart();
@@ -21,7 +27,7 @@ const Shop = () => {
         // step 1: get id of the addedProduct
         for (const id in storedCart) {
             // step 2: get product from products state by using id
-            const addedProduct = products.find(product => product.id === id)
+            const addedProduct = products.find(product => product._id === id)
             if (addedProduct) {
                 // step 3: add quantity
                 const quantity = storedCart[id];
@@ -41,19 +47,19 @@ const Shop = () => {
         // const newCart = [...cart, product];
         // if product doesn't exist in the cart, then set quantity = 1
         // if exist update quantity by 1
-        const exists = cart.find(pd => pd.id === product.id);
+        const exists = cart.find(pd => pd._id === product._id);
         if (!exists) {
             product.quantity = 1;
             newCart = [...cart, product]
         }
         else {
             exists.quantity = exists.quantity + 1;
-            const remaining = cart.filter(pd => pd.id !== product.id);
+            const remaining = cart.filter(pd => pd._id !== product._id);
             newCart = [...remaining, exists];
         }
 
         setCart(newCart);
-        addToDb(product.id)
+        addToDb(product._id)
     }
 
     const handleClearCart = () => {
@@ -62,27 +68,41 @@ const Shop = () => {
     }
 
     return (
-        <div className='shop-container'>
-            <div className="products-container">
+        <>
+            <div className='shop-container'>
+                <div className="products-container">
+                    {
+                        products.map(product => <Product
+                            key={product._id}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        ></Product>)
+                    }
+                </div>
+                <div className="cart-container">
+                    <Cart
+                        cart={cart}
+                        handleClearCart={handleClearCart}
+                    >
+                        <Link className='proceed-link' to="/orders">
+                            <button className='btn-proceed'>Review Order</button>
+                        </Link>
+                    </Cart>
+                </div>
+            </div>
+            <div className='pagination'>
+                <p> selected page : {selectedPageNumber}</p>
+
                 {
-                    products.map(product => <Product
-                        key={product.id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
+                    [...Array(totalPages).keys()].map(pageNumber => <button 
+                    onClick={()=> setSelectedPageNumber(pageNumber)}    
+                    className={pageNumber === selectedPageNumber? 'active' : '' }
+                    key={pageNumber}
+                    
+                    >{pageNumber}</button>)
                 }
             </div>
-            <div className="cart-container">
-                <Cart
-                    cart={cart}
-                    handleClearCart={handleClearCart}
-                >
-                    <Link className='proceed-link' to="/orders">
-                        <button className='btn-proceed'>Review Order</button>
-                    </Link>
-                </Cart>
-            </div>
-        </div>
+        </>
     );
 };
 
